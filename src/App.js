ddad8524995d14e4b2a7afd497ab4651ff0e24aa
renanberton <renan_berton@yahoo.com.br>
2024-06-28 14:22:20 -0300

    Ajustes e componetização da página sobre

--- src/App.js
+++ src/App.js
@@ -1,55 +1,10 @@
 import './App.scss';
 import Home from './pages/Home/Home';
+import About from './pages/About/About';
 
 function App() {
-
-const Skill = ({ name, rating }) => {
   return (
-    <div className="skill">
-      <span className="skill-name">{name}</span>
-      <div className="stars">
-        {Array.from({ length: 5 }, (_, index) => (
-          <span key={index} className={index < rating ? "star filled" : "star"}>★</span>
-        ))}
-      </div>
-    </div>
-  );
-};
-
-
-const skillsData = [
-  { name: 'HTML', rating: 3 },
-  { name: 'CSS / SASS', rating: 4 },
-  { name: 'Javascript', rating: 4 },
-  { name: 'React', rating: 5 },
-  { name: 'Wordpress', rating: 4 },
-  { name: 'Bancos de Dados', rating: 4 },
-  { name: 'Tailwind / Bootstrap', rating: 5 },
-  { name: 'Design UX / UI', rating: 4 },
-  { name: 'API\'s e Requisições', rating: 4 },
-];
-
-const Skills = () => {
-  return (
-    <div className="box-skills">
-      <img src="assets/images/rehzera.png" alt="Foto do Renan" />
-      <h1>RENAN ALEXANDRE BERTON</h1>
-      <span className='text-about'>Formado em Análise e Desenvolvimento de Sistemas na Universidade Paulista.
-      <br /><br /> Atuo com Desenvolvimento focado em Front-End e algumas tecnologias back-end e bancos de dados.</span>
-      <div>
-        <h2>Tecnologias</h2>
-      </div>
-      <div className="skills-container">
-        {skillsData.map((skill, index) => (
-          <Skill key={index} name={skill.name} rating={skill.rating} />
-        ))}
-      </div>
-    </div>
-  );
-};
-
-  return (
-    <Skills />
+    <Home />
   );
 }
 
