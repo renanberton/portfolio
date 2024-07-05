44188323da49025fe4b3d1058d3f5bae213513f9
renanberton <renan_berton@yahoo.com.br>
2024-07-05 16:02:10 -0300

    Página Experiências feita e responsiva

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.scss';
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
+import Experiences from './pages/Experiences/Experiences';
 
 function App() {
   return (
-    <Home />
+    < About />
   );
 }
 
---
+++ src/pages/Experiences/Experiences.js
@@ -0,0 +1,27 @@
+import './style.scss'
+
+function Experiences() {
+   return (
+      <div className='container-experience'>
+      <h1>Experiências</h1>
+      <div className="box-experiences">
+      <h2>Estagiário de Desenvolvimento Full-Stack</h2>    
+      <div className="job-informations">
+         <h3>Doois Web</h3>
+         <span>Out. de 2021 / Out. de 2022 </span>
+      </div>
+         <p>Desenvolvimento e modificações de Websites responsivos e funcionais.Uso de Wordpress, HTML, CSS, Javascript, React, Python, MySql e Linux, Projetos de UX/UI com Figma.Ferramentas Bitbucket, Jira, Strapi, etc.Versionamento de códigos com GIT/Github</p>
+      </div>
+      <div className="box-experiences">
+      <h2>Estagiário de Desenvolvimento Full-Stack</h2>    
+      <div className="job-informations">
+         <h3>Doois Web</h3>
+         <span>Out. de 2021 / Out. de 2022 </span>
+      </div>
+         <p>Desenvolvimento e modificações de Websites responsivos e funcionais.Uso de Wordpress, HTML, CSS, Javascript, React, Python, MySql e Linux, Projetos de UX/UI com Figma.Ferramentas Bitbucket, Jira, Strapi, etc.Versionamento de códigos com GIT/Github</p>
+      </div>
+   </div>
+   )
+}
+
+export default Experiences;
