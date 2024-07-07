71ff3b79f2d34e45539467eae40b6ea17dd987ab
renanberton <renan_berton@yahoo.com.br>
2024-07-07 16:28:33 -0300

    Implementado botão Scroll To Top

--- src/App.js
+++ src/App.js
@@ -4,8 +4,28 @@ import About from './pages/About/About';
 import Experiences from './pages/Experiences/Experiences';
 import Projects from './pages/Projects/Projects';
 import Contact from './pages/Contact/Contact';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [showButton, setShowButton] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 300) {
+        setShowButton(true);
+      } else {
+        setShowButton(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
   return (
     <div>
       <Home />
@@ -13,8 +33,12 @@ function App() {
       <Experiences />
       <Projects />
       <Contact />
+      {showButton && (
+        <button className="scroll-to-top" onClick={scrollToTop}>
+          ↑
+        </button>
+      )}
     </div>
-        
   );
 }
 
