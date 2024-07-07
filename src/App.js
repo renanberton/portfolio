c765430c8d27f2e6baf52eda6d676d8a278311f4
renanberton <renan_berton@yahoo.com.br>
2024-07-07 12:42:25 -0300

    Página de projetos feita + Responsividade

--- src/App.js
+++ src/App.js
@@ -5,7 +5,77 @@ import Experiences from './pages/Experiences/Experiences';
 
 function App() {
   return (
-    < About />
+    <div className='container-projects'>
+      <h3>Projetos</h3>
+      <div className="test">
+        <div className="box-projects">
+          <img className='img-project' src="/assets/images/placeholder.png" alt="Imagem do projeto" />
+          <h4>Nome do Projeto</h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+          <a href="/">
+            <div className='box-veja-mais'>
+              <span>Veja Mais</span>
+              <img src="/assets/images/arrow-right 1.png" alt="Seta para a Direita" />
+            </div>
+          </a>
+        </div>
+        <div className="box-projects">
+          <img className='img-project' src="/assets/images/placeholder.png" alt="Imagem do projeto" />
+          <h4>Nome do Projeto</h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+          <a href="/">
+            <div className='box-veja-mais'>
+              <span>Veja Mais</span>
+              <img src="/assets/images/arrow-right 1.png" alt="Seta para a Direita" />
+            </div>
+          </a>
+        </div>
+        <div className="box-projects">
+          <img className='img-project' src="/assets/images/placeholder.png" alt="Imagem do projeto" />
+          <h4>Nome do Projeto</h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+          <a href="/">
+            <div className='box-veja-mais'>
+              <span>Veja Mais</span>
+              <img src="/assets/images/arrow-right 1.png" alt="Seta para a Direita" />
+            </div>
+          </a>
+        </div>
+        <div className="box-projects">
+          <img className='img-project' src="/assets/images/placeholder.png" alt="Imagem do projeto" />
+          <h4>Nome do Projeto</h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+          <a href="/">
+            <div className='box-veja-mais'>
+              <span>Veja Mais</span>
+              <img src="/assets/images/arrow-right 1.png" alt="Seta para a Direita" />
+            </div>
+          </a>
+        </div>
+        <div className="box-projects">
+          <img className='img-project' src="/assets/images/placeholder.png" alt="Imagem do projeto" />
+          <h4>Nome do Projeto</h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+          <a href="/">
+            <div className='box-veja-mais'>
+              <span>Veja Mais</span>
+              <img src="/assets/images/arrow-right 1.png" alt="Seta para a Direita" />
+            </div>
+          </a>
+        </div>
+        <div className="box-projects">
+          <img className='img-project' src="/assets/images/placeholder.png" alt="Imagem do projeto" />
+          <h4>Nome do Projeto</h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+          <a href="/">
+            <div className='box-veja-mais'>
+              <span>Veja Mais</span>
+              <img src="/assets/images/arrow-right 1.png" alt="Seta para a Direita" />
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
   );
 }
 
