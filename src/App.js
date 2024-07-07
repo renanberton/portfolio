5f9fa169cbedd68f44e95e7cdfb7b888c987aeea
renanberton <renan_berton@yahoo.com.br>
2024-07-07 14:21:33 -0300

    Ajustes de Responsividade

--- src/App.js
+++ src/App.js
@@ -2,79 +2,15 @@ import './App.scss';
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
 import Experiences from './pages/Experiences/Experiences';
+import Projects from './pages/Projects/Projects';
 
 function App() {
   return (
-    <div className='container-projects'>
-      <h3>Projetos</h3>
-      <div className="test">
-        <div className="box-projects">
-          <img className='img-project' src="/assets/images/placeholder.png" alt="Imagem do projeto" />
-          <h4>Nome do Projeto</h4>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
-          <a href="/">
-            <div className='box-veja-mais'>
-              <span>Veja Mais</span>
-              <img src="/assets/images/arrow-right 1.png" alt="Seta para a Direita" />
-            </div>
-          </a>
-        </div>
-        <div className="box-projects">
-          <img className='img-project' src="/assets/images/placeholder.png" alt="Imagem do projeto" />
-          <h4>Nome do Projeto</h4>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
-          <a href="/">
-            <div className='box-veja-mais'>
-              <span>Veja Mais</span>
-              <img src="/assets/images/arrow-right 1.png" alt="Seta para a Direita" />
-            </div>
-          </a>
-        </div>
-        <div className="box-projects">
-          <img className='img-project' src="/assets/images/placeholder.png" alt="Imagem do projeto" />
-          <h4>Nome do Projeto</h4>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
-          <a href="/">
-            <div className='box-veja-mais'>
-              <span>Veja Mais</span>
-              <img src="/assets/images/arrow-right 1.png" alt="Seta para a Direita" />
-            </div>
-          </a>
-        </div>
-        <div className="box-projects">
-          <img className='img-project' src="/assets/images/placeholder.png" alt="Imagem do projeto" />
-          <h4>Nome do Projeto</h4>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
-          <a href="/">
-            <div className='box-veja-mais'>
-              <span>Veja Mais</span>
-              <img src="/assets/images/arrow-right 1.png" alt="Seta para a Direita" />
-            </div>
-          </a>
-        </div>
-        <div className="box-projects">
-          <img className='img-project' src="/assets/images/placeholder.png" alt="Imagem do projeto" />
-          <h4>Nome do Projeto</h4>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
-          <a href="/">
-            <div className='box-veja-mais'>
-              <span>Veja Mais</span>
-              <img src="/assets/images/arrow-right 1.png" alt="Seta para a Direita" />
-            </div>
-          </a>
-        </div>
-        <div className="box-projects">
-          <img className='img-project' src="/assets/images/placeholder.png" alt="Imagem do projeto" />
-          <h4>Nome do Projeto</h4>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
-          <a href="/">
-            <div className='box-veja-mais'>
-              <span>Veja Mais</span>
-              <img src="/assets/images/arrow-right 1.png" alt="Seta para a Direita" />
-            </div>
-          </a>
-        </div>
-      </div>
+    <div>
+      <Home />
+      <About />
+      <Experiences />
+      <Projects />
     </div>
   );
 }
---
+++ src/pages/Projects/Projects.js
@@ -0,0 +1,79 @@
+import './style.scss'
+
+function Projects() {
+   return (
+      <div className='container-projects'>
+      <h3>Projetos</h3>
+      <div className="test">
+        <div className="box-projects">
+          <img className='img-project' src="/assets/images/placeholder.png" alt="Imagem do projeto" />
+          <h4>Nome do Projeto</h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+          <a href="/">
+            <div className='box-veja-mais'>
+              <span>Veja Mais</span>
+              <img src="/assets/images/arrow-right 1.png" alt="Seta para a Direita" />
+            </div>
+          </a>
+        </div>
+        <div className="box-projects">
+          <img className='img-project' src="/assets/images/placeholder.png" alt="Imagem do projeto" />
+          <h4>Nome do Projeto</h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+          <a href="/">
+            <div className='box-veja-mais'>
+              <span>Veja Mais</span>
+              <img src="/assets/images/arrow-right 1.png" alt="Seta para a Direita" />
+            </div>
+          </a>
+        </div>
+        <div className="box-projects">
+          <img className='img-project' src="/assets/images/placeholder.png" alt="Imagem do projeto" />
+          <h4>Nome do Projeto</h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+          <a href="/">
+            <div className='box-veja-mais'>
+              <span>Veja Mais</span>
+              <img src="/assets/images/arrow-right 1.png" alt="Seta para a Direita" />
+            </div>
+          </a>
+        </div>
+        <div className="box-projects">
+          <img className='img-project' src="/assets/images/placeholder.png" alt="Imagem do projeto" />
+          <h4>Nome do Projeto</h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+          <a href="/">
+            <div className='box-veja-mais'>
+              <span>Veja Mais</span>
+              <img src="/assets/images/arrow-right 1.png" alt="Seta para a Direita" />
+            </div>
+          </a>
+        </div>
+        <div className="box-projects">
+          <img className='img-project' src="/assets/images/placeholder.png" alt="Imagem do projeto" />
+          <h4>Nome do Projeto</h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+          <a href="/">
+            <div className='box-veja-mais'>
+              <span>Veja Mais</span>
+              <img src="/assets/images/arrow-right 1.png" alt="Seta para a Direita" />
+            </div>
+          </a>
+        </div>
+        <div className="box-projects">
+          <img className='img-project' src="/assets/images/placeholder.png" alt="Imagem do projeto" />
+          <h4>Nome do Projeto</h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+          <a href="/">
+            <div className='box-veja-mais'>
+              <span>Veja Mais</span>
+              <img src="/assets/images/arrow-right 1.png" alt="Seta para a Direita" />
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+   )
+}
+
+export default Projects;
