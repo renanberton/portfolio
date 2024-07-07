00afa2d47c3f3388ee9b265384b446465e02b49b
renanberton <renan_berton@yahoo.com.br>
2024-07-07 16:39:37 -0300

    Adicionados o telefone, email e linkedin nas ancoras do contato

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -6,16 +6,16 @@ function Contact() {
         <h2>Contatos</h2>
         <p>Gostou do meu perfil? <br /> Entre em contato!</p>
         <div className="box-contacts">
-          <a href="/" target="_blank" rel="noopener noreferrer">
+          <a href="mailto:renan_berton@yahoo.com.br" target="_blank" rel="noopener noreferrer">
             <img src="/assets/images/mail.png" alt="Logo E-mail" />
           </a>
-          <a href="/" target="_blank" rel="noopener noreferrer">
+          <a href="tel:+5511976573355" target="_blank" rel="noopener noreferrer">
             <img src="/assets/images/tell.png" alt="Logo Telefone" />
           </a>
-          <a href="/" target="_blank" rel="noopener noreferrer">
+          <a href="https://www.linkedin.com/in/renanberton/" target="_blank" rel="noopener noreferrer">
             <img src="/assets/images/Linkedin.png" alt="Logo Linkedin" />
           </a>
-          <a href="/" target="_blank" rel="noopener noreferrer">
+          <a href="https://wa.me/5511976573355" target="_blank" rel="noopener noreferrer">
             <img src="/assets/images/whatsapp.png" alt="Logo Whatsapp" />
           </a>
         </div>
